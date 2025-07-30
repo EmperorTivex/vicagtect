abe3f4db1cf77a64d86cedfa72bd39eafdbc34a2
Victor-kukoyi Oluwatise <victorkukoyioluwatise@gmail.com>
2025-07-30 11:26:52 +0100

    Update to my Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,14 @@
 import Herosection from "../components/Herosection";
 import Constructionsection from "../components/Constructionsection";
+import Investor from "../components/Investor";
+import Investor from "../components/Investor";
 
 function Home() {
   return (
     <div>
       <Herosection />
       <Constructionsection />
+      <Investor />
     </div>
   );
 }
